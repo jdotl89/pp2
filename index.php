e5068a2a4286f288f8a1f95632c5ca87cd374184
John Laygo <jlaygo@mail.greenriver.edu>
2020-04-17 16:32:17 -0400

    Added header, title, and body content to index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+//Turn on error reporting -- this is critical!
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+/* Name: John Laygo
+   Date: 04/17/2020
+   URl:
+   Pair Program 2
+*/
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program</title>
+</head>
+<body>
+<h1>Pair Program 2</h1>
+<?php
+
+echo "PHP Array Practice";
+
+?>
+
+</body>
+</html>
